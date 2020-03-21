722a9403dd522fc500fa1ada7cf0d4da818f7edb
Kholkin <kholkins@mail.ru>
2020-03-21 18:54:59 +0300

    Add videoView

--- app/src/main/java/com/example/usingvideo/MainActivity.java
+++ app/src/main/java/com/example/usingvideo/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.usingvideo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+    VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        videoView = (VideoView)findViewById(R.id.videoView);
+
+
+
     }
+
 }
